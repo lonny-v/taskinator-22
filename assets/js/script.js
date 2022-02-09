97031b4a27e084bfa4b77aa95c6061aafd53e901
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-02-09 10:25:24 -0700

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -222,45 +222,20 @@ var saveTasks = function () {
 // convert tasks from string back to array of objects
 // Iterate through a tasks array and creates task elements on the page from it
 var loadTasks = function () {
-    var tasks = localStorage.getItem("tasks");
-    console.log(tasks);
-    if (!tasks) {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
         return false;
     }
+    
+    savedTasks = JSON.parse(savedTasks);
 
-    tasks = JSON.parse(tasks);
-    // console.log(tasks);
-    for (var i = 0; i < tasks.length; i++) {
-
-        taskIdCounter = tasks[i].id;
-        // console.log(tasks[i])
-
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", taskIdCounter);
-        // console.log(listItemEl)
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-        console.log(listItemEl);
-
-        if (tasks[i].status === "to-do") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        } else if (tasks[i].status === "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        } else if (tasks[i].status === "completed") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        // increase task counter for next unique id
-    taskIdCounter++;
-    console.log(listItemEl)
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the `createTaskEl()` function
+        createTaskEl(savedTasks[i]);
     }
-};
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
